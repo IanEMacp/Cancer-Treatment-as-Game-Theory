5d44eda39a111c281f6b54358f309ff5cac58b2d
ten18 <48300794+ten18@users.noreply.github.com>
2019-04-09 14:12:15 -0400

    Added more code outline and guidelines

--- Main.py
+++ Main.py
@@ -1,21 +1,29 @@
-#Execute the entire program from this file
+# Execute the entire program from this file. This code isn't complete.
 
-#import ModelReplica as MR (or whatever our calculative file is called)
+import Functions as funct # We need Functions.py for this to run.
 
-def Main(): #Insert all of our math variable names into the parentheses
+def Main(): # Insert all of our used variable names from bottom line
+            # into the parentheses.
     print("How many times?")
     while True:
         try:
-            n = int(inpit())
+            n = int(input())
         except ValueError:
-            print("Please type a numeric value.")
+            print("Please type a numeric value.") # Error check.
             print("")
             continue
         else:
             break
 
     cancerPayoffList = []
-    oncologistPayoffList = []
+    oncologistPayoffList = [] 
 
-    for x in range(n):
-        tumorPayoff, oncologistPayoff, endSituation = MR
+    for x in range(n): # Game runs for however many times the user says.
+        tumorPayoff, oncologistPayoff, endSituation = funct.Game
+
+# Put visual feedback as print statements here.    
+
+# This will run our entire program
+Main() # <-- Set variable values into parentheses (ex: 10, 0.5, 20).
+       # If variable is user-manipulated, set variable as a string that asks
+       # for input.
