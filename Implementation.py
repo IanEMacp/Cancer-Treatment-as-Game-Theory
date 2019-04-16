fac1fc07a26e368698529d9c4b7a724ec31434cb
IanEMacp <mandalorian.beskar@gmail.com>
2019-04-16 09:13:21 -0400

    pretty much everything

--- Implementation.py
+++ Implementation.py
@@ -1,56 +1,160 @@
 
 import random as r
+import math as m
 
 class Cell:
-  def __init__(self, health = 100, resistence1 = 0, resistence2 = 0, mortalityRate = 20):
+  def __init__(self, health = 100, resistence1 = 0, resistence2 = 0, mortalityRate = 10):
         self.health = health
         self.resistence1 = resistence1
         self.resistence2 = resistence2
         self.mortalityRate = mortalityRate
 
   def age(self):
-    self.health = self.health - self.mortalityRate
-    return self
+    if (self.health - self.mortalityRate > 0):
+      return Cell(health = self.health - self.mortalityRate, resistence1 = self.resistence1, resistence2 = self.resistence2, mortalityRate = self.mortalityRate)
 
   def mutate(self):
-    change = r.randrange(1, 5, 1)
+    change = r.randrange(1, 6, 1)
     if(change == 1):
       self.resistence1 += 1
     if(change == 2):
       self.resistence2 += 1
     if(change == 3):
-      self.resistence1 -= 1
+      if(self.resistence1 > 0):
+        self.resistence1 -= 1
     if(change == 4):
-      self.resistence2 -= 1
+      if(self.resistence2 > 0):
+        self.resistence2 -= 1
     return self
 
-  def reproduce(self):
-    if(r.randrange(1, 5, 1) == 1):
-      return [self, self]
-    return self
+  def reproduce(self, childHealth):
+    oldSelf = Cell(health = self.health, resistence1 = self.resistence1, resistence2 = self.resistence2, mortalityRate = self.mortalityRate)
+    rand = r.randrange(1, 6, 1)
+    if(rand == 1 or rand == 2):
+      child = Cell(health = childHealth + 20, resistence1 = self.resistence1, resistence2 = self.resistence2, mortalityRate = self.mortalityRate)
+      return [oldSelf, child]
+    return [oldSelf]
 
-    
+  def treat(self, drug):
+    rand1 = r.randrange(1, 4 + self.resistence1, 1)
+    rand2 = r.randrange(1, 4 + self.resistence2, 1)
+    if (drug == 1 & (rand1 != 1 and rand1 != 2)):
+      return Cell(health = self.health, resistence1 = self.resistence1, resistence2 = self.resistence2, mortalityRate = self.mortalityRate)
+    if (drug == 2 & (rand2 != 1 and rand2 != 2)):
+      return Cell(health = self.health, resistence1 = self.resistence1, resistence2 = self.resistence2, mortalityRate = self.mortalityRate)
+    if (self.health - 20 > 0):
+      return Cell(health = self.health - 20, resistence1 = self.resistence1, resistence2 = self.resistence2, mortalityRate = self.mortalityRate)
+
+
 class Tumor:
 
-  def __init__(self, initialPopulation=list(), carryCapacity=100, growthRate=10):
-        self.carryCapacity = carryCapacity
-        self.population = initialPopulation
-        self.growthRate = growthRate
+  def __init__(self, initialPopulationSize = 100, health = 100, resistence1 = 0, resistence2 = 0, mortalityRate = 20):
+        self.childHealth = health
+        self.population = list()
+        for i in range(1, initialPopulationSize + 1):
+          self.population.append(Cell(health - (m.floor(health/5) * m.ceil(i/m.ceil(initialPopulationSize/5)) - m.floor(health/5)), resistence1, resistence2, mortalityRate))
 
   def age(self):
-    self.population = list(map(lambda cell: cell.age(), self.population))
+    self.population = list(filter(None, map(lambda cell: cell.age(), self.population)))
 
   def reproduce(self):
-    self.population = flatten(list(map(lambda cell: cell.reproduce(), self.population)))
+    self.population = sum(list(map(lambda cell: cell.reproduce(self.childHealth), self.population)), [])
 
   def mutate(self):
     self.population = list(map(lambda cell: cell.mutate(), self.population))
 
+  def treat(self, drug):
+    self.population = list(filter(None, map(lambda cell: cell.treat(drug), self.population)))
 
-if __name__ == "__main__": # All values currently shown below are just test values.
-                           # I don't know which of these will actually be decimals.
-    TumorMortality(100,0.5,10,0.2,0.3,0.5,0.5,1)
-    GFunction(0.5,100,10,1,0.2,0.3,0.5,0.5,1)
-    LinearTradeoff(0.3)
-    # Not gonna bother running the last two functions. Program shows they run
-    # just fine.
+
+def trial(cycles = 13, treatmentStrategy = [], maxTumorSize = 1000, tumorFailureSize = True):
+#cycles is the number of 4 week treatment cycles to simulate
+#treatmentStrategy must be list containing only 1s and 2s or an empty list for no treatment
+  tumor = Tumor()
+  for i in range(1, cycles + 1):
+    if(treatmentStrategy):
+      tumor.treat(treatmentStrategy[i % len(treatmentStrategy)])
+    if(len(tumor.population) < maxTumorSize):
+      tumor.reproduce()
+    tumor.mutate()
+    tumor.age()
+    if(maxTumorSize < len(tumor.population) & tumorFailureSize):
+      print("failed at " + str(i))
+      return
+  
+  print("success: ")
+  print("popsize: " + str(len(tumor.population)))
+  g = list()
+  for x in tumor.population:
+    g.append(x.health)
+  print("health: " + str(sum(g)/len(g)))
+  return len(tumor.population)
+
+
+def ptrial(cycles = 13, treatmentStrategy = [], maxTumorSize = 1000, tumorFailureSize = True):
+#same as trial but with some extra print outs
+  tumor = Tumor()
+  for i in range(1, cycles + 1):
+    print("cycle: " + str(i))
+    print("popsize")
+    print(len(tumor.population))
+    print("health")
+    g = list()
+    for x in tumor.population:
+      g.append(x.health)
+    print(sum(g)/len(g))
+    if(treatmentStrategy):
+      tumor.treat(treatmentStrategy[i % len(treatmentStrategy)])
+    if(len(tumor.population) < maxTumorSize):
+      tumor.reproduce()
+    tumor.mutate()
+    tumor.age()
+    if(maxTumorSize < len(tumor.population) & tumorFailureSize):
+      print("failed at " + str(i))
+      return
+  print("success")
+  print("popsize: " + str(len(tumor.population)))
+  g = list()
+  for x in tumor.population:
+    g.append(x.health)
+  print("health: " + str(sum(g)/len(g)))
+  return len(tumor.population)
+
+def nptrial(cycles = 13, treatmentStrategy = [], maxTumorSize = 1000, tumorFailureSize = True):
+#same as trial but with no print outs
+  tumor = Tumor()
+  for i in range(1, cycles + 1):
+    if(treatmentStrategy):
+      tumor.treat(treatmentStrategy[i % len(treatmentStrategy)])
+    if(len(tumor.population) < maxTumorSize):
+      tumor.reproduce()
+    tumor.mutate()
+    tumor.age()
+    if(maxTumorSize < len(tumor.population) & tumorFailureSize):
+      return 1000
+  return len(tumor.population)
+
+#print("strat = []:")
+#trial(cycles=39)
+#print("-----------------------------")
+#print("strat = [1]:")
+#trial(cycles=39, treatmentStrategy = [1])
+#print("-----------------------------")
+#print("strat = [1, 2]:")
+#trial(cycles=39, treatmentStrategy = [1, 2])
+#print("-----------------------------")
+#print("strat = [1, 2, 1, 2, 2]:")
+#trial(cycles=39, treatmentStrategy = [1, 2, 1, 2, 2])
+#print("-----------------------------")
+
+def compareStrats(strat1, strat2):
+  #percentage that strat1 is better
+  results = list()
+  for x in range(1,51):
+    if(nptrial(cycles=39, treatmentStrategy = strat1) < nptrial(cycles=39, treatmentStrategy = strat2)):
+      results.append(True)
+    if(nptrial(cycles=39, treatmentStrategy = strat1) > nptrial(cycles=39, treatmentStrategy = strat2)):
+      results.append(False)
+  return sum(results)/len(results)
+
+print(compareStrats([1, 2, 1, 2, 2], [1, 2]))
